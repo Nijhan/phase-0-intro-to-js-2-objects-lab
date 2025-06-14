e7bbfa6594cdddd255d2b186b4e2d8667fd9dbeb
Nijhan <najmashaaban6@gmail.com>
2025-06-14 19:55:56 +0300

    objects

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee ={
+    name:"Najma",
+streetAdress:"Ngong"
+}
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    return {
+        ...employee,
+        [key]:value
+    }
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key]=value
+    return employee
+}
+function deleteFromEmployeeByKey(employee,key){
+    let newemployee={...employee}
+delete newemployee[key]
+return newemployee
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key]
+    return employee
+}
